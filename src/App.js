0a9891f2c9075454f3aa9f581b378b3247932027
rizkyzet <rizkyzetzet121@gmail.com>
2022-02-15 08:03:20 +0700

    fix nav

--- src/App.js
+++ src/App.js
@@ -158,7 +158,10 @@ function App() {
               <div className='flex justify-start gap-1 items-center'>
                 <NavLink className={({ isActive }) =>
                   isActive ? activeClass : standartClass
-                } to="/">
+                } to="/" onClick={()=>{
+                  setSearchMode(false)
+                  setSearchKeyword('');
+                }}>
                   {/* <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
                 </svg> */}
@@ -210,7 +213,10 @@ function App() {
               <div className='flex justify-start gap-1 items-center'>
                 <NavLink className={({ isActive }) =>
                   isActive ? activeClass : standartClass
-                } to="/">
+                } to="/" onClick={()=>{
+                  setSearchMode(false)
+                  setSearchKeyword('');
+                }}>
                   {/* <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
                 </svg> */}
@@ -243,7 +249,10 @@ function App() {
               <div className='flex justify-start gap-1 items-center'>
                 <NavLink className={({ isActive }) =>
                   isActive ? activeClass : standartClass
-                } to="/">
+                } to="/" onClick={()=>{
+                  setSearchMode(false)
+                  setSearchKeyword('');
+                }}>
                   {/* <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
                 </svg> */}
